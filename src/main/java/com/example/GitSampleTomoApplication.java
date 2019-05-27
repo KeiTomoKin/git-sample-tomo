3fba8ee8abab24a60756f4d42656170153233aa5
tomookakeita <keita516tomo@outlook.jp>
2019-05-27 12:24:16 +0900

    iプロジェクト新規作成

---
+++ src/main/java/com/example/GitSampleTomoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleTomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleTomoApplication.class, args);
+	}
+
+}
