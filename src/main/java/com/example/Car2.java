0050e23141862f9f993cdced7c165707663a171a
tomookakeita <keita516tomo@outlook.jp>
2019-05-27 13:44:29 +0900

    Carクラスを作成

---
+++ src/main/java/com/example/Car2.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class Car2 {
+	private int speed;
+	private String bodyColor;
+	
+	public Car2(int speed, String bodyColor) {
+		super();
+		this.speed = speed;
+		this.bodyColor = bodyColor;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getBodyColor() {
+		return bodyColor;
+	}
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+}
